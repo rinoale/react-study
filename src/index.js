36a5a024a9d39485adb2fe6a48fd70aceccacd5f
rinoale <rinoale@hanmail.net>
2017-06-27 15:56:29 +0900

    kanban list components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Hello from './components/Hello';
-import GroceryList from './components/GroceryList';
+import KanbanBoard from './components/KanbanBoard';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<GroceryList />, document.getElementById('root'));
+const cardsList = [
+  {
+    id: 1,
+    title: 'Read the Book',
+    description: 'I should read the whole book',
+    status: 'in-progress',
+    tasks: [],
+  },
+  {
+    id: 1,
+    title: 'Write some code',
+    descripton: 'Code along with the samples in the book',
+    status: 'todo',
+    tasks: [
+      {
+        id: 1,
+        name: 'ContactList Example',
+        done: true,
+      },
+      {
+        id: 2,
+        name: 'Kanban Example',
+        done: false,
+      },
+      {
+        id: 3,
+        name: 'My own experiments',
+        done: false,
+      },
+    ],
+  },
+];
+
+ReactDOM.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
 registerServiceWorker();
